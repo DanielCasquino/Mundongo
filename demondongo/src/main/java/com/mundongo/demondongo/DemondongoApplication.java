09615b22a1e54a1a11d41faa610d3b9ba65fb674
Danee2108 <daniel.casquino@utec.edu.pe>
2023-10-24 19:17:07 -0500

    Initial commit

---
+++ demondongo/src/main/java/com/mundongo/demondongo/DemondongoApplication.java
@@ -0,0 +1,13 @@
+package com.mundongo.demondongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemondongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemondongoApplication.class, args);
+	}
+
+}
