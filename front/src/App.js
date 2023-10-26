678bfe2a3d31a8fd08fe40a68b4ca53c2c189a73
Danee2108 <daniel.casquino@utec.edu.pe>
2023-10-26 12:01:25 -0500

    You spin me right round baby right round

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,16 @@
 import { useState } from 'react';
+import earth from './world.svg';
+
+function LoginButton() {
+  return <button className="loginButton">Log In</button>;
+}
 
 function LoginBox() {
   return (
     <div className="loginBox">
-      <div className="inputField">email</div>
-      <div className="inputField">password</div>
+      <div className="inputField">Email</div>
+      <div className="inputField">Password</div>
+      <LoginButton />
     </div>
   );
 }
@@ -13,8 +19,16 @@ export default function body() {
   return (
     <div className="body">
       <div className="appWrapper">
-        <div className="loginBackground"></div>
+        <div className="loginBackground">
+          <div className="planetWrapper">
+            <div className="planet">
+              <img className="earth" src={earth}></img>
+              <div className="planetShadow"></div>
+            </div>
+          </div>
+        </div>
         <div className="loginWrapper">
+          <div className="title">MUNDONGO</div>
           <LoginBox />
         </div>
       </div>
