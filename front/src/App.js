097d5c9fbb2518093782eda2a62b4893daafaf5f
Danee2108 <daniel.casquino@utec.edu.pe>
2023-10-26 15:08:17 -0500

    Added stars

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import earth from './world.svg';
 
 function LoginButton() {
@@ -20,8 +20,10 @@ export default function body() {
     <div className="body">
       <div className="appWrapper">
         <div className="loginBackground">
+          <Stars />
           <div className="planetWrapper">
             <div className="planet">
+              <img className="earth" src={earth}></img>
               <img className="earth" src={earth}></img>
               <div className="planetShadow"></div>
             </div>
@@ -34,4 +36,30 @@ export default function body() {
       </div>
     </div>
   );
+}
+
+function Stars() {
+  useEffect(() => {
+    createRandomStars();
+  }, []);
+
+  const createRandomStars = () => {
+    const container = document.querySelector('.starWrapper');
+    const starNumber = 100;
+    for (let i = 0; i < starNumber; i++) {
+      const star = document.createElement('div');
+      star.classList.add('star');
+      const xOffset = Math.random() * 100;
+      const yOffset = Math.random() * 100;
+      const animDelay = Math.random() * 5 * (Math.random() / 2);
+      const duration = 0.5 + Math.random();
+      star.style.left = `${xOffset}%`;
+      star.style.top = `${yOffset}%`;
+      star.style.animationDelay = `${animDelay}s`;
+      star.style.setProperty('--duration', `${duration}s`);
+      container.appendChild(star);
+    }
+  };
+
+  return <div className="starWrapper"></div>;
 }
