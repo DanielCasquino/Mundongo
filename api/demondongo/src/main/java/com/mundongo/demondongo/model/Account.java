4c24fcd62b8b9b043b649c97ba5eb453e0edcc62
snah <camiloernestosoto@gmail.com>
2023-10-31 16:15:12 -0500

    🚧  Initial part of security

--- api/demondongo/src/main/java/com/mundongo/demondongo/model/Account.java
+++ api/demondongo/src/main/java/com/mundongo/demondongo/model/Account.java
@@ -1,25 +1,25 @@
 package com.mundongo.demondongo.model;
 
-import java.util.Set;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import java.util.*;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "account")
-public class Account {
+public class Account implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+    @Column(unique=true)
     private String email;
     private String name;
     private String lastName;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @OneToMany
     private Set<Comment> comments;
@@ -27,10 +27,12 @@ public class Account {
     public Account() {
     }
 
-    public Account(String email, String name, String lastName) {
+    public Account(String email, String name, String lastName, String password, Role role) {
         this.email = email;
         this.name = name;
         this.lastName = lastName;
+        this.password = password;
+        this.role = role;
     }
 
     public Long getId() {
@@ -41,7 +43,8 @@ public class Account {
         this.id = id;
     }
 
-    public String getEmail() {
+    @Override
+    public String getUsername() {
         return this.email;
     }
 
@@ -67,5 +70,39 @@ public class Account {
 
     public void addComment(Comment comment) {
         this.comments.add(comment);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
     }
 }
