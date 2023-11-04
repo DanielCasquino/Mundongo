c8e72fddf9a1fa6f44b1ae758b2a381482bf56a1
DanielCasquino <103433199+DanielCasquino@users.noreply.github.com>
2023-11-03 22:52:22 -0500

    Improved discover page mockup

--- front/src/pages/discover/discover.js
+++ front/src/pages/discover/discover.js
@@ -6,16 +6,64 @@ import dawg from './F1-ZVoKXsAIW8EU.jpg';
 
 function SearchBar(){
   return(
-    <input className='searchBar' placeholder='Start typing to search for a cool place!'></input>
+    <input className='searchBar' placeholder='Start typing to search for cool stuff!'></input>
   );
 }
 
-function CollapseButton({ onClick, status}) {
+function CollapseButton({ onClick, status }) {
   return (
     <button className='collapseButton' onClick={onClick}>
       <img className={status ? "collapseImage" : "collapseImage rotated"} src={arrow} alt="Arrow" />
     </button>
   );
+}
+
+function RandomTags(){
+  const randomTags = [];
+  const tagNumber = Math.floor(Math.random() * 3) + 1;
+  for (let i = 0; i < tagNumber; ++i){
+    const rand = Math.floor(Math.random() * 3) + 1;
+    let color = "";
+    let text = "Tag " + i;
+    switch(rand){
+      case 1: color = "var(--tag-fuchsia)"; break;
+      case 2: color = "var(--tag-grass)"; break;
+      case 3: color = "var(--tag-lime)"; break;
+    }
+    const tagStyle = {
+      '--data': `${color}`,
+    };
+    randomTags.push(<div key={i} className='tag' style={tagStyle}>{text}</div>)
+  }
+  return randomTags;
+}
+
+function Card({title, location}){
+  return(
+    <div className='card'>
+      <div className='text'>
+        <div className='name'>{title}</div>
+        <div className='location'>{location}</div>
+      </div>
+      <div className='thumbnail'>
+        <img className="image" src='https://cataas.com/cat'></img>
+      </div>
+      <div className='tags'>
+      {<RandomTags/>}
+      </div>
+    </div>
+  )
+}
+
+function CardRow({rowIndex}){
+  const cards = [];
+  for (let i = 0; i < 3; ++i){
+    let cardIndex = 3 * rowIndex + i;
+    cards.push(<Card key={i} title={"Event " + cardIndex} location={"City, Country"}/>)
+  }
+  return(
+    <div className='cardRow'>{cards}</div>
+  )
 }
 
 export default function body() {
@@ -27,6 +75,11 @@ export default function body() {
   }
 
   let leftClass = collapsedBar ? "left" : "left closed";
+
+  const cardRows = [];
+  for (let i = 0; i < 4; ++i){
+    cardRows.push(<CardRow key={i} rowIndex={i}/>);
+  }
 
   return (
     <div className="body discover">
@@ -42,6 +95,7 @@ export default function body() {
               <CollapseButton onClick={() => showBar()} status={collapsedBar}/>
               <SearchBar/>
               </div>
+              <div className='cardContainer'>{cardRows}</div>
             </div>
           </div>
         </div>
